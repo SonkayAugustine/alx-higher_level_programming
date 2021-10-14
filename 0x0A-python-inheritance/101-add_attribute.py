93a53844e1071c8ff943673ffdcafb9672013353
SonkayAugustine <zamanisonkay@gmail.com>
2021-10-14 02:32:57 +0000

    Update

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -6,10 +6,9 @@ Task 13 (101) Module
 
 def add_attirbute(obj, name, value):
     '''
-    Check if it is possible to add an attribute to a class
-    and then if yes add it
+    Check if it is possible to add an attr to class
     '''
     if '__dict__' not in dir(obj):
+       object.__dict__[name] = value
+    else:
         raise TypeError("cant't add new attribute")
-
-        setattr(obj, name, value)
