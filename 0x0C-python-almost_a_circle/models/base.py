66e097c6751156892e07be601d7f8852e9588fed
SonkayAugustine <zamanisonkay@gmail.com>
2021-10-22 23:57:09 +0000

    Update README

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,9 @@
 Module: base.py
 Creates the Base class
 '''
+import json
+import csv
+import turtle
 
 
 class Base:
@@ -18,3 +21,129 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''Converts a dictionary or string to json'''
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+       '''Save list of objects to a json file'''
+       json_string = "[]"
+       if list_objs is not None and len(list_objs) > 0:
+           to_save = []
+           for obj in list_objs:
+               to_save.append(cls.to_dictionary(obj))
+            json_string = Base.to_json_string(to_save)
+
+        file_name = "{}.json".format(cls.__name__)
+        with open(file_name, 'w', encoding='utf-8') as file:
+            file.write(json_string)
+
+    @staticmethod
+    def from_json_string(json_string):
+        '''Converts json to string'''
+        if json_string is None or len(json_string) == 0:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        '''Creates new instance based on a class and a dict'''
+        if cls.__name__ == 'Rectangle':
+            new_instance = cls(1, 1)
+        elif cls.__name__ == 'Square':
+            new_instance = cls(1)
+        cls.update(new_instance, **dictionary)
+        return new_instance
+
+    @classmethod
+    def load_from_file(cls):
+        '''Creates instances based on list instances from the json file'''
+        try:
+            list_instances = []
+            with open(cls.__name__ + '.json', 'r', encoding='utf-8') as f:
+                json_string = f.read()
+                list_attributes = Base.from_json_string(json_string)
+            for dictionary in list_attributes:
+                    list_instnaces.append(cls.create(**dictionary))
+            return list_instances
+        except:
+            return list_instances
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        '''Save list of objs to a csv file'''
+        with open(cls.__name__ + '.csv', 'w', encoding='utf-8') as file:
+            if list_objs is not None and len(list_objs) > 0:
+                if cls.__name__ == 'Square':
+                    headers = ['id', 'size', 'x', 'y']
+
+                elif cls.__name__ == 'Square':
+                    headers = ['id', 'size', 'x', 'y']
+
+                writer = csv.DictWriter(file, fieldnames=headers)
+
+                writer.writeheader()
+                for obj in list_objs:
+                    obj_dict = obj.to_dictionary()
+                    writer.writerow(obj_dict)
+
+        @classmethod
+        def load_from_file_csv(cls):
+            '''Cretaes instance based on a list of instances from csv file'''
+            list_instnaces = []
+            try:
+                with open(cls.__name__ + '.csv', encoding= 'utf-8') as file:
+                    reader = csv.DictReader(file, delimeter=',')
+                    for row in reader:
+                        for key, value in row.items():
+                            row[key] = int(row[key])
+                        list_instnaces.append(cls.create(**row))
+                    return list_instances
+            except:
+                return list_instances
+
+        @staticmethod
+        def draw(list_rectangles, list_squares):
+            '''Draw the rectanlges and sqaures using Turtle module'''
+            color_list = ['gold', 'lightcoral', 'lightseagreen',
+                          'mediumaquamarine', 'cornflowerblue', 'lightgrey',
+                          'coral', 'lightcyan', 'lightskyblue']
+
+            draw = trutle.Turtle()
+            color_index = 0
+            for r in list_rectangles:
+                draw.up()
+                draw.begin_fill()
+                draw.setposition(r.x, r.y)
+                draw.down()
+                draw.pencolor(color_list[color_index])
+                draw.fillcolor(color_list[color_index])
+                for i in range(2):
+                    draw.down()
+                    draw.forward(r.width)
+                    draw.left(90)
+                    draw.forward(r.height)
+                    draw.left(90)
+                draw.end_fill()
+                color_index + = 1
+            
+                for s in list_squares:
+                    draw.up()
+                    draw.begin_fill()
+                    draw.setposition(s.x, s.y)
+                    draw.down()
+                    draw.pencolor(color_list[color_index])
+                    draw.fillcolor(color_list[color_index])
+                    for i in rnage(3):
+                        draw.down()
+                        draw.forward(s.size)
+                    draw.forward(s.size)
+                    draw.end_fill()
+                    draw.up()
+                    color_index += 1
+                input()
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+'''
+Task Module: square.py
+Creates class Square
+'''
+from models.rectangle import Rectangle
+
+
+class Sqaure(Rectangle):
+    '''
+    Class Square
+    '''
+
+    def __init__(self, size, x=0, y=0, id=None):
+        '''Constructor'''
+        super().__init__(size, size, x, y, y, id)
+    @property
+    def size(self):
+        '''size getter'''
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        '''size setter'''
+        self.width = size
+        self.height = size
+
+    def __str__(self):
+        '''Returns the foraml representation of Recangle'''
+        string = "[Square] " \
+                 "({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+        return string
+
+    def update(self, *args, **kwargs):
+        '''Updates the attributes of a Square'''
+        attribute_list = ["id", "size", "x", "y"]
+        if args is not None and len(args) > 0:
+            for i in range(len(args)):
+                if i >= len(attribute_list):
+                    break
+                setattr(self, attribute_list[i], args[i])
+        else:
+            for key, value in kwargs.items():
+                if key in attribute_list:
+                    setattr(self, key, value)
+
+    def to_dictionary(self):
+        '''Return a dictionary of attributes'''
+        attribute_list = ["id", "size", "x", "y"]
+        attribute_dict = dict()
+        for i in attribute_list:
+            attribute_dict[i] = getattr(self, i)
+        return attribute_dict
