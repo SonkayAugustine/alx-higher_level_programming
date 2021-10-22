14880f19a1b1873e86c7a7eafabbb0f6af6a68d3
SonkayAugustine <zamanisonkay@gmail.com>
2021-10-22 19:13:28 +0000

    Update README

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+Module: base.py
+Creates the Base class
+'''
+
+
+class Base:
+    '''
+    The Base class
+    '''
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        '''Constructor'''
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
