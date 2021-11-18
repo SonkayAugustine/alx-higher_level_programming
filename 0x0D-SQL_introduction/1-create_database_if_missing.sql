0aaa600369c76b3238439f34d30901798aa46a4e
SonkayAugustine <zamanisonkay@gmail.com>
2021-11-18 16:20:44 +0000

    fixed

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
--- A script that creates htbn_0c_0 database in MySQL server
-CREATE DATABASE IF NOT EXISTS htbn_0c_0;
+-- script that creates hbtn_0c_0 database in MySQL server
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
