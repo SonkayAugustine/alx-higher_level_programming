41833a746a6a333d374076cc430a0a7a305d8ee5
SonkayAugustine <zamanisonkay@gmail.com>
2021-12-14 18:04:54 +0000

    Update

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+function myFunc (i) {
+  const myArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+  for (let i = 0; i < myArr.length; i++) {
+    console.log(myArr[i]);
+  }
+}
+myFunc();
