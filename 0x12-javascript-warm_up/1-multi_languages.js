9bafc388513ad438836fcecc0fe252538da4105b
SonkayAugustine <zamanisonkay@gmail.com>
2021-12-14 12:53:20 +0000

    Added

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const firstLine = 'C is fun';
+const secondLine = 'Python is cool';
+const thirdLine = 'Javascript is amazing';
+
+function myFunc (a) {
+  console.log(a);
+}
+
+myFunc(firstLine);
+myFunc(secondLine);
+myFunc(thirdLine);
