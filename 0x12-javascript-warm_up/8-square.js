a6b37758c145f1e36e55e116f56ee1335a669926
SonkayAugustine <zamanisonkay@gmail.com>
2021-12-15 14:13:52 +0000

    Update

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const args = process.argv.slice(1);
+const errMsg = 'Missing size';
+const check = Number(args[1]);
+let square = '';
+
+if (!check) {
+  console.log(errMsg);
+} else {
+  const c = parseInt(check);
+  for (let i = 0; i < c; i++) {
+    for (let j = 0; j < c; j++) {
+      square += 'X';
+    }
+    square += '\n';
+  }
+  console.log(square);
+}
