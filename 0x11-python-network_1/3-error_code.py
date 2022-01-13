cc3b066da5eb304f917f8b799356ba264a379e37
SonkayAugustine <zamanisonkay@gmail.com>
2022-01-13 05:45:53 -0800

    fixed task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,11 +5,10 @@ Take in a URL, send a request to URL, and dispaly body
 
 if __name__ == '__main__':
     import sys
-    import urllib.request
-    import urllib.error
+    from urllib import request, error
 
     try:
-        with urllib.request.urlopen(sys.argv[1]) as res:
+        with request.urlopen(sys.argv[1]) as res:
             print(res.read().decode('utf-8'))
     except urllib.error.HTTPError as er:
-        print("Error code:".format(er.code))
+        print("Error code:",(er.code))
