86bcb17151ff2f896033a525b9dd7ff3fdc9f445
SonkayAugustine <zamanisonkay@gmail.com>
2022-01-13 04:49:53 -0800

    fixed task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 # Python script that fetches https://intranet.hbtn.io/status
-import urllib.request
+
 
 if __name__ == "__main__":
+    import urlib.request
+    
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
         content = res.read()
         print("Body response:")
