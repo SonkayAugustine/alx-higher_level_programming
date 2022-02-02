1e44ea64d21590233d1c1bb6aeb0e41bad5fc593
SonkayAugustine <zamanisonkay@gmail.com>
2022-02-01 16:08:45 -0800

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,9 +3,9 @@
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', function (err, file) {
-    if (err) {
-	return console.log(err);
-    } else {
-	console.log(file);
-    }
+  if (err) {
+    return console.log(err);
+  } else {
+    console.log(file);
+  }
 });
